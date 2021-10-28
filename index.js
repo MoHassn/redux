d8b41502b61539c374d6ea99f3fc93b359d3ad42
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2021-10-28 14:11:45 +0200

    simple implementation of Redux

---
+++ index.js
@@ -0,0 +1,184 @@
+// helper functions
+function generateId() {
+  return (
+    Math.random().toString(36).substring(2) + new Date().getTime().toString(36)
+  );
+}
+
+// library code
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    return () => (listeners = listeners.filter((li) => li !== listener));
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
+
+// app code
+
+// actions
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+// action creators
+
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  };
+}
+
+// reducers
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat(action.todo);
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id ? todo : { ...todo, done: !todo.done }
+      );
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat(action.goal);
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  };
+}
+
+const store = createStore(app);
+store.subscribe(() => console.log(`the new state is`, store.getState()));
+store.subscribe(() => {
+  const { todos, goals } = store.getState();
+
+  // clear the DOM
+  document.getElementById("todos").innerHTML = "";
+  document.getElementById("goals").innerHTML = "";
+
+  todos.forEach(addTodoToDOM);
+  goals.forEach(addGoalToDOM);
+});
+
+// DOM code
+function addTodo() {
+  const input = document.getElementById("todo");
+  const text = input.value;
+  input.value = "";
+
+  store.dispatch(
+    addTodoAction({
+      id: generateId(),
+      done: false,
+      text,
+    })
+  );
+}
+function addGoal() {
+  const input = document.getElementById("goal");
+  const text = input.value;
+  input.value = "";
+
+  store.dispatch(
+    addGoalAction({
+      id: generateId(),
+      text,
+    })
+  );
+}
+
+document.getElementById("todoBtn").addEventListener("click", addTodo);
+
+document.getElementById("goalBtn").addEventListener("click", addGoal);
+
+function createRemoveButton(onClickHandler) {
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", onClickHandler);
+  return button;
+}
+
+function addTodoToDOM(todo) {
+  const node = document.createElement("li");
+  node.innerText = todo.text;
+  if (todo.done) {
+    node.classList.add("done");
+  }
+
+  const removeButton = createRemoveButton(() => {
+    store.dispatch(removeTodoAction(todo.id));
+  });
+  node.addEventListener("click", (e) => {
+    store.dispatch(toggleTodoAction(todo.id));
+  });
+  node.appendChild(removeButton);
+  document.getElementById("todos").appendChild(node);
+}
+
+function addGoalToDOM(goal) {
+  const node = document.createElement("li");
+  node.innerText = goal.text;
+  const removeButton = createRemoveButton(() =>
+    store.dispatch(removeGoalAction(goal.id))
+  );
+  node.appendChild(removeButton);
+  document.getElementById("goals").appendChild(node);
+}
